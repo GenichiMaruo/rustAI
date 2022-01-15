c7a42a8d10e77bad412f61488b566dd5603cced8
GenichiMaruo <gen1010f8e7@gmail.com>
2022-01-15 21:40:23 +0900

    addとexpの追加

--- src/functions.rs
+++ src/functions.rs
@@ -4,3 +4,17 @@ pub fn square(x: super::Variable) -> crate::Variable{
     println!("{}",y.data);
     return y;
 }
+
+pub fn add(x: super::Variable, y: super::Variable) -> crate::Variable{
+    let z = super::Variable::new();
+    z.data = x.data + x.data;
+    println!("{}",z.data);
+    return y;
+}
+
+pub fn exp(x: super::Variable) -> crate::Variable{
+    let y = super::Variable::new();
+    y.data = x.data * x.data;
+    println!("{}",y.data);
+    return y;
+}
