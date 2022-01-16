c1ec34cec56d916020b52a419622af3ab5d74ee7
GenichiMaruo <gen1010f8e7@gmail.com>
2022-01-16 21:21:23 +0900

    expの関数を仮に用意

--- src/functions.rs
+++ src/functions.rs
@@ -12,9 +12,11 @@ pub fn add(x: super::Variable, y: super::Variable) -> crate::Variable{
     return y;
 }
 
-pub fn exp(x: super::Variable) -> crate::Variable{
-    let y = super::Variable::new();
-    y.data = x.data * x.data;
-    println!("{}",y.data);
-    return y;
+pub fn exp(x: super::Variable, y: i32) -> crate::Variable{
+    let z = super::Variable::new();
+    for n in 1..y {
+        z.data = z.data * x.data;
+    }
+    println!("{}",z.data);
+    return z;
 }
